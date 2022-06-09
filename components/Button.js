4c3a0d4ff01bc6b17ff1f1e97e284654cbde0dd4
rickmuch <ricardojrmuchacho@gmail.com>
2022-06-09 07:28:26 -0500

    apuntes clase

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,29 @@
 const template = document.createElement("template");
+
+//el css debe importarse dinamicamente no aqui, igual con el div y buton hay que cambiarlo
+//a document.createElement como sale abajo
 template.innerHTML = `
 <style>
+div{
+  display: flex;
+  justify-content: center;
+}
   button {
       color: black;
   }
 </style>
-
+<div>
 <button/>
-
+</div>
 `;
 
 class Button extends HTMLElement {
   constructor() {
     super();
+
+    //this.container = document.createElement('div');
+    //this.button = document.createElement('button');
+    //this.container.appendChild(this.button)
 
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
